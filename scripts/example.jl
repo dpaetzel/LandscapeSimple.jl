690deea63e2ebf0c6616dd50023734625c8f4452
David Pätzel <david.paetzel@posteo.de>
2024-07-20 18:37:53 +0200

    📝 Improve example

--- scripts/example.jl
+++ scripts/example.jl
@@ -7,17 +7,28 @@ hps_vary = (;
     b=mkscale_minmax(1.0f-3, 5.0f-2),
 )
 
-# Generate `2^m` configurations deterministically. For now, we never run
-# more than 1024 configs.
+# Generate `2^m` configurations deterministically.
 m = 5
 configs = configurations(Random.Xoshiro(31), hps_vary; m=m)
 
 # If you have UnicodePlots available:
 try
+    using StatsBase
     using UnicodePlots
-    local a = map(first, configs)
-    local b = map(last, configs)
-    UnicodePlots.scatterplot(a, b)
-catch
-    println("Skipping UnicodePlots stuff")
+
+    println("Uniformly sampled tends to clump:")
+    a_ = sample([3, 5, 8, 12, 17, 23], 2^m)
+    b_ = rand(2^m) .* (5.0f-2 - 1.0f-3) .+ 1.0f-3
+    display(UnicodePlots.scatterplot(Float64.(a_), b_))
+
+    println("Sobol' sequence more evenly covers space:")
+    a = map(first, configs)
+    b = map(last, configs)
+    display(UnicodePlots.scatterplot(a, b))
+catch e
+    if e isa ArgumentError
+        println("Skipping StatsBase/UnicodePlots stuff")
+    else
+        rethrow()
+    end
 end
