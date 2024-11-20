c79945a743a2eec6a60310a26fcb0e70b8cf3ef8
David Pätzel <david.paetzel@posteo.de>
2024-11-20 16:15:12 +0100

    📌 Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,30 +1,60 @@
 {
   inputs = {
-    nixpkgs.url = "github:dpaetzel/nixpkgs/dpaetzel/nixos-config";
+    nixpkgs.url =
+      # "github:dpaetzel/nixpkgs/dpaetzel/nixos-config";
+      "github:dpaetzel/nixpkgs/update-clipmenu";
     systems.url = "github:nix-systems/default";
     devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+
+    overlays.url = "github:dpaetzel/overlays/master";
   };
 
   nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
-    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
-    in {
-      devShells = forEachSystem (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in rec {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      devenv,
+      systems,
+      overlays,
+      ...
+    }@inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              overlays.overlays.mydefaults
+            ];
+          };
+        in
+        rec {
           default = devenv.lib.mkShell {
             inherit inputs pkgs;
-            modules = [{
-              # https://devenv.sh/reference/options/
-              languages.julia.enable = true;
-            }];
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                languages.julia.enable = true;
+                languages.julia.package = pkgs.myjulia;
+
+                scripts.repl.exec = ''
+                  julia --project=.
+                '';
+              }
+            ];
           };
           devShell.${system} = default;
-        });
+        }
+      );
     };
 }
